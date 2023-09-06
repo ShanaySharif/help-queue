f05210eeadab5e801eceb449f2dfdced3284a153
ShanaySharif <Shanay.uw17@gmail.com>
2023-09-06 18:02:36 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import TicketControl from "./TicketControl";
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <TicketControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/TicketControl.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+class TicketControl extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          formVisibleOnPage: false
+        };
+      }
+      handleClick = () => {
+        this.setState({formVisibleOnPage: true});
+      }
+
+  render(){
+    let currentlyVisibleState = null;
+    let addTicketButton = null;;
+    if (this.state.formVisibleOnPage) {
+        currentlyVisibleState = <NewTicketForm />
+        // buttonText = "Return to Ticket List";
+    } else {
+        currentlyVisibleState = <TicketList />
+        addTicketButton = <button onClick={this.handleClick}>add ticket </button>
+        addTicketButton = <button onClick={this.handleClick}>add ticket </button>
+        addTicketButton = <button onClick={this.handleClick}>add ticket </button>
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        {addTicketButton}
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TicketControl;
